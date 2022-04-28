bcf9f792a584f29a242f04f43fd0ca1a4b32f5be
吴莉蓉 <wulr@eastcom-sw.com>
2022-04-28 10:52:46 +0800

    提交1

---
+++ vueproject/src/components/AsideMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+      <el-radio-button :label="false">展开</el-radio-button>
+      <el-radio-button :label="true">收起</el-radio-button>
+    </el-radio-group>
+    <el-menu
+      default-active="1-4-1"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      :collapse="isCollapse"
+    >
+    
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AsideMenu",
+  components: {},
+  props: {},
+  data() {
+    return {
+
+    }
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style scoped lang="less">
+</style>
